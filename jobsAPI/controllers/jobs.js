451424e75325c03173d9458fed4fc1069fdb1eed
Naveen Jha <naveen.jha@argusoft.com>
2026-08-07 18:35:30 +0530

    Work of 7th August

---
+++ jobsAPI/controllers/jobs.js
@@ -0,0 +1,31 @@
+const Job=require('../models/Job');
+const {BadRequestError,NotFoundError}=require('../errors');
+const { StatusCodes } = require('http-status-codes');
+
+const getAllJobs= async (req ,res)=>{
+   const jobs=await Job.find({createdBy:req.user.userId}).sort('createdAt');
+   res.status(StatusCodes.OK).json({jobs,count:jobs.length});
+}
+const getJob= async (req ,res)=>{
+    res.send('get job')
+}
+const createJob= async (req ,res)=>{
+    req.body.createdBy=req.user.userId;
+    const job=await Job.create(req.body);
+    
+    res.status(StatusCodes.CREATED).json({job})
+
+}
+const deleteJob= async (req ,res)=>{
+    res.send('delete job')
+}
+const updateJob= async (req ,res)=>{
+    res.send('update job')
+}
+module.exports={
+    getAllJobs,
+    getJob,
+    createJob,
+    deleteJob,
+    updateJob,
+}
---
+++ jobsAPI/middleware/authentication.js
@@ -0,0 +1,22 @@
+const User=require('../models/User');
+const jwt=require('jsonwebtoken');
+const {UnauthenticatedError}=require('../errors');
+const auth=(req,res,next)=>{
+    const authHeader=req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')){
+        throw new UnauthenticatedError("Authentication Invalid");
+    }
+    const token=authHeader.split(' ')[1]
+try {
+    const payload=jwt.verify(token,process.env.JWT_SECRET);
+    
+    const user=User.findById(payload.id).select('-password');
+    req.user=user;
+
+    req.user={userId:payload.userId,name: payload.name}
+    next();
+}catch (error){
+   throw new UnauthenticatedError("Authentication Invalid");
+}
+}
+module.exports=auth;
